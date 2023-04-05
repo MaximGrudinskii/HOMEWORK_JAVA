c260d8d9e8a0f26b72f7f2b644f5cff18e6c225d
Maksim <grudinskij97@mail.ru>
2023-04-05 21:03:19 +0800

    HW1

---
+++ HW1/Zadanie1.java
@@ -0,0 +1,32 @@
+package HW1;
+
+/**
+ * Zadanie1
+ *  Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! (произведение чисел от 1 до n)
+ * Ввод:5
+ * Треугольное число 1 + 2 + 3 + 4 + 5 = 15
+ * n! 1 * 2 * 3 * 4 * 5 = 120
+ */
+
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        System.out.printf("n!: %d\n", getFactorial(i));
+        iScanner.close();
+    }
+    public static int giveMeNumber(int a) {
+        return  (a * (a + 1)) / 2;
+    }
+    public static int getFactorial(int b) {
+        int result = 1;
+        for (int c = 1; c <= b; c++) {
+            result = result * c;
+        }
+        return result;
+    }
+}
