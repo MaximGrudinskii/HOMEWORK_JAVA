21902762e131b56792f57870026cd23a245b4265
Maksim <grudinskij97@mail.ru>
2023-04-05 21:00:26 +0800

    HW1

---
+++ HW1/Zadanie2.java
@@ -0,0 +1,23 @@
+package HW1;
+
+/**
+ * Zadanie2
+ * Вывести все простые числа от 1 до 1000
+ */
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int number = 1000;
+        int counter = 0;
+        System.out.printf("Простые числа в диапозоне от 1 до %d:\n", number);
+        for (int i = 2; i <= number; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0) counter++;
+            }
+            if (counter == 1) {
+                System.out.println(i);
+            } 
+            counter = 0;
+        }
+    }
+}
